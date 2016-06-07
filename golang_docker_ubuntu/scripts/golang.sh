5b5ffc69fe174929ac8dae1bebc993179b65182f
john arroyo <john.arroyo@gmail.com>
2016-06-07 02:25:56 -0700

    got the glide ubuntu image / container working
    woo hoo

--- golang_docker_ubuntu/scripts/golang.sh
+++ golang_docker_ubuntu/scripts/golang.sh
@@ -6,12 +6,4 @@ curl -O https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
 tar -xvf go1.6.linux-amd64.tar.gz
 mv go /usr/local
 
-# Export path & add to profile so it gets set automatically
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=/go
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-echo 'export GOPATH=/go' >> ~/.profile
-source ~/.profile
-
 echo "Golang installed"
-echo $GOPATH
